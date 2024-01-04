42645e484d23a78be8452810c76f144b1bc2c9b1
jang hun han <84340857+janhan42@users.noreply.github.com>
2024-01-04 12:58:08 +0900

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/janhan42/Raycasting.git
-cd Rayacasting
+cd Raycasting
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/janhan42/SFML-installer/master/installer.sh)"
 mkdir .OBJ
 make
